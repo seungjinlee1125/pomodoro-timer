9647b6fdcf1febb38dbdb881c6b89967c880c7bc
seungjinlee <seungjinlee@iseungjin-ui-MacBook-Pro.local>
2018-09-24 16:03:22 -0700

    Setup with expo for React Native Development

---
+++ components/button/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, TouchableOpacity, StyleSheet} from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+
+
+function Button({iconName, onPress}){
+    return(
+        <TouchableOpacity onPressOut = {onPress}>
+            <FontAwesome name={iconName} size={80} color='white'/>
+        </TouchableOpacity>
+    )
+}
+
+Button.propType = {
+    iconName : PropTypes.string.isRequired,
+    onPress : PropTypes.func.isRequired,
+};
+
+export default Button;
